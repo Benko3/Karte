a4054689d21043d2e588e602c2872e44447534ba
Benko3 <152974533+Benko3@users.noreply.github.com>
2023-12-05 13:03:41 +0100

    Add files via upload

---
+++ Karte.cs
@@ -0,0 +1,37 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int karte, bodovi = 0;
+		while (bodovi < 31)
+		{
+			Console.Write("Upisite vrijednost karte:");
+			try
+			{
+			karte = Convert.ToInt32(Console.ReadLine());
+			if (karte > 13 || karte < 1)
+			{
+				Console.WriteLine("Pogrešan unos!");
+				continue;
+			}
+			else
+			{
+				bodovi += karte;
+		    }
+		}
+		catch (Exception ex) {
+			Console.WriteLine(ex.Message);
+		
+		    }
+        }
+		if (bodovi == 31)
+		{
+			Console.WriteLine("Bodovi:" + bodovi);
+			Console.WriteLine("Izgubili ste!");
+		}
+		
+	}
+	
+}
